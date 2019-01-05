bebdc6e0d1d961ee78b9208db19fd53ad4b0c26d
Arsenii <a.derkach@douleutaras.com>
2019-01-06 01:43:12 +0200

    test heroku connection

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
-const TelegramBot = require('node-telegram-bot-api');
+// const TelegramBot = require('node-telegram-bot-api');
+
+// // replace the value below with the Telegram token you receive from @BotFather
+// const token = '328596384:AAGPSAUF3dp1X5uN3XJ3rwXJuibH8z_AUxw';
+
+// // Create a bot that uses 'polling' to fetch new updates
+// const bot = new TelegramBot(token, {polling: true});
+
+// // const timezone = 2;
+
+// const deadline = new Date(1547132820000);
+
+// const hoursLeft = () => Math.floor((deadline-Date.now())/(1000*60*60));
+// const minutesLeft = () => Math.floor((deadline-Date.now())/(1000*60))%60;
+// const secondsLeft = () => Math.floor((deadline-Date.now())%(1000))%60;
+
+// bot.onText(/\/time_left/, function timeLeft(msg) {
+//     bot.sendMessage(msg.chat.id, `Осталось ${hoursLeft()} часов, ${minutesLeft()} минут и ${secondsLeft()} секунд `);
+//   });
+
+  
+// bot.on('message', (msg) => {
+//     if (~msg.text.indexOf('time_left')) {
+        
+//     } else  {
+//         const chatId = msg.chat.id;
+
+//         // send a message to the chat acknowledging receipt of their message
+//         bot.sendMessage(chatId, 'Пока что не работаем, но скоро будем)');
+//     }
+  
+// });
+
+
+/**
+ * This example demonstrates setting up webhook
+ * on the Heroku platform.
+ */
+
 
 const TOKEN = process.env.TELEGRAM_TOKEN || '328596384:AAGPSAUF3dp1X5uN3XJ3rwXJuibH8z_AUxw';
-// const TelegramBot = require('../..');
+const TelegramBot = require('../..');
 const options = {
   webHook: {
     // Port to which you should bind is assigned to $PORT variable
@@ -25,30 +63,7 @@ const bot = new TelegramBot(TOKEN, options);
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
 
-// Create a bot that uses 'polling' to fetch new updates
-// const bot = new TelegramBot(token, {polling: true});
-
-// const timezone = 2;
-
-const deadline = new Date(1547132820000);
-
-const hoursLeft = () => Math.floor((deadline-Date.now())/(1000*60*60));
-const minutesLeft = () => Math.floor((deadline-Date.now())/(1000*60))%60;
-const secondsLeft = () => Math.floor((deadline-Date.now())%(1000))%60;
-
-bot.onText(/\/time_left/, function timeLeft(msg) {
-    bot.sendMessage(msg.chat.id, `Осталось ${hoursLeft()} часов, ${minutesLeft()} минут и ${secondsLeft()} секунд `);
-  });
-
-  
-bot.on('message', (msg) => {
-    if (~msg.text.indexOf('time_left')) {
-        
-    } else  {
-        const chatId = msg.chat.id;
-
-        // send a message to the chat acknowledging receipt of their message
-        bot.sendMessage(chatId, 'Пока что не работаем, но скоро будем)');
-    }
-  
+// Just to ping!
+bot.on('message', function onMessage(msg) {
+  bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
 });
