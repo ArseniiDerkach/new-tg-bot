06c8ed8f17f6b02ab67aa04d4d76d3307ded5f24
Arsenii Derkach <arsenii.derkach@revjet.com>
2019-03-17 21:15:55 +0200

    edited timestamp

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const bot = new TelegramBot(token, {polling: true});
 // const timezone = 2;
 
 const deadline = new Date(1547132820000);
-const trainDeadline = new Date(1547153580000);
+const trainDeadline = new Date(1552850088000);
 
 const hoursLeft = (date) => ((deadline - date)>0) ? Math.floor((deadline-date)/(1000*60*60)) : Math.floor((trainDeadline-date)/(1000*60*60));
 const minutesLeft = (date) => ((deadline - date)>0) ? Math.floor((deadline-date)/(1000*60))%60 : Math.floor((trainDeadline-date)/(1000*60))%60;
