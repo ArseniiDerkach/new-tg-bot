9d5900c41992fc727a13fc40d66a72248f1cb631
Arsenii Derkach <arsenii.derkach@revjet.com>
2019-05-18 10:45:24 +0300

    fixing time

--- app.js
+++ app.js
@@ -14,16 +14,16 @@ const token = '328596384:AAGPSAUF3dp1X5uN3XJ3rwXJuibH8z_AUxw';
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-bot.sendMessage(70698447, `bot is up!}`);
+bot.sendMessage(70698447, `bot is up!`);
 
 // const timezone = 2;
 
 const deadline = new Date(1558164207000);
-const trainDeadline = new Date(1553317800000);
+const trainDeadline = new Date(1558164207000);
 
-const hoursLeft = (date) => ((deadline - date)>0) ? Math.floor((deadline-date)/(1000*60*60)) : Math.floor((trainDeadline-date)/(1000*60*60));
-const minutesLeft = (date) => ((deadline - date)>0) ? Math.floor((deadline-date)/(1000*60))%60 : Math.floor((trainDeadline-date)/(1000*60))%60;
-const secondsLeft = (date) => ((deadline - date)>0) ? Math.floor((deadline-date)/(1000))%60 : Math.floor((trainDeadline-date)/(1000))%60;
+const hoursLeft = (date) => ((deadline - date)>0) ? Math.floor((deadline-date)/(1000*60*60)) : Math.floor((deadline-date)/(1000*60*60));
+const minutesLeft = (date) => ((deadline - date)>0) ? Math.floor((deadline-date)/(1000*60))%60 : Math.floor((deadline-date)/(1000*60))%60;
+const secondsLeft = (date) => ((deadline - date)>0) ? Math.floor((deadline-date)/(1000))%60 : Math.floor((deadline-date)/(1000))%60;
 
 bot.onText(/\/time_left/, function timeLeft(msg) {
     let currentDate = new Date();
